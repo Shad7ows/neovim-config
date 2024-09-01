8b79b65afe97be94514c3ae23a84274451fcbbc3
Shadow <77246874+Shad7ows@users.noreply.github.com>
2024-09-01 11:07:22 +0300

    Update debugging.lua

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,15 +1,54 @@
 return {
 	"mfussenegger/nvim-dap",
 	dependencies = {
-		"leoluz/nvim-dap-go",
+		"nvim-neotest/nvim-nio",
 		"rcarriga/nvim-dap-ui",
 	},
 	config = function()
-    require("dapui").setup()
-		require("dap-go").setup()
-
+    		require("dapui").setup()
 		local dap, dapui = require("dap"), require("dapui")
 
+		dap.adapters.gdb = {
+		  type = "executable",
+		  command = "gdb",
+		  args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+		}
+		dap.configurations.cpp = {
+		  {
+		    name = "Launch",
+		    type = "gdb",
+		    request = "launch",
+		    program = function()
+		      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		    end,
+		    cwd = "${workspaceFolder}",
+		    stopAtBeginningOfMainSubprogram = false,
+		  },
+		  {
+		    name = "Select and attach to process",
+		    type = "gdb",
+		    request = "attach",
+		    program = function()
+		       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		    end,
+		    pid = function()
+		       local name = vim.fn.input('Executable name (filter): ')
+		       return require("dap.utils").pick_process({ filter = name })
+		    end,
+		    cwd = '${workspaceFolder}'
+		  },
+		  {
+		    name = 'Attach to gdbserver :1234',
+		    type = 'gdb',
+		    request = 'attach',
+		    target = 'localhost:1234',
+		    program = function()
+		       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+		    end,
+		    cwd = '${workspaceFolder}'
+		  },
+		}
+		
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
@@ -22,10 +61,5 @@ return {
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 		end
-
-		vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
 	end,
 }
