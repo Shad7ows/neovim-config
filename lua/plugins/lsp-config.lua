74d980b1774dcf16885ef793b1027a673ee2b298
Shadow <77246874+Shad7ows@users.noreply.github.com>
2024-09-01 11:09:26 +0300

    Update lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,13 +20,7 @@ return {
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
+      lspconfig.clangd.setup({
         capabilities = capabilities
       })
 
